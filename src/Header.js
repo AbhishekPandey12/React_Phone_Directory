2490458ca7291c8d58ad64fdc2001a1a43e5f054
abhishekkumarpandey <abhisonu3629@gmail.com>
2019-07-20 21:54:44 +0530

    Created ‘Header’ Component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = function (params) {
+    return(
+        <div className="header">
+            Phone Directory
+        </div>
+    );
+}
+
+export default Header;
